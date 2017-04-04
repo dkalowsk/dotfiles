8cff2e269ecc5353ceb321ab89bfc78cf39b27c9
Dan Kalowsky <dank@deadmime.org>
2017-04-03 17:48:40 -0700

    ycm: initial import of extra_conf.py
    Initial import of the ycm_extra_conf.py used as a global file
    
    Signed-off-by: Dan Kalowsky <dank@deadmime.org>

---
+++ ycm_extra_conf.py
@@ -0,0 +1,38 @@
+def FlagsForFile(filename, **kwargs):
+
+  flags = [
+    '-Wall',
+    '-Wextra',
+    '-Werror'
+    '-pedantic',
+    '-I',
+    '.',
+    '-isystem',
+    '/usr/include',
+    '-isystem',
+    '/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include',
+    '-isystem',
+    '/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/8.1.0/include',
+    '-isystem',
+    '/usr/local/include',
+    '-isystem',
+    '/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/c++/v1',
+  ]
+
+  data = kwargs['client_data']
+  filetype = data['&filetype']
+
+  if filetype == 'c':
+    flags += ['-xc']
+  elif filetype == 'cpp':
+    flags += ['-xc++']
+    flags += ['-std=c++14']
+  elif filetype == 'objc':
+    flags += ['-ObjC']
+  else:
+    flags = []
+
+  return {
+    'flags':    flags,
+    'do_cache': True
+  }
