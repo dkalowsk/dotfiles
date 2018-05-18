ef463e1eff319c770c7049940f31edf983e89298
Dan Kalowsky <dank@deadmime.org>
2018-05-18 11:28:50 -0700

    fzf: autocomplete enable by sourcing default files
    Previously I was doing all the work for enabling auto-complete, which
    worked fine.  It was also unnecessary.
    
    A lot of the work for auto-complete is already maintained by the fzf
    project, and provided in a bash-complete file.  Sourcing that file will
    accomplish the same thing.
    
    Signed-off-by: Dan Kalowsky <dank@deadmime.org>

--- fzf/.fzf.bash
+++ fzf/.fzf.bash
@@ -1,30 +1,14 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == *${HOME}/.fzf/bin* ]]; then
-  export PATH="$PATH:${HOME}/.fzf/bin"
+if [[ ! "$PATH" == *$HOME/.fzf/bin* ]]; then
+  export PATH="$PATH:$HOME/.fzf/bin"
 fi
 
-# Auto-completion & key-bindings
+# Auto-completion
 # ---------------
-if [ "$OSTYPE" == "darwin"* ]; then
-	if [ -e $(brew --prefix)/opt/fzf/shell/completion.bash ]; then
-		source $(brew --prefix)/opt/fzf/shell/key-bindings.bash
-		source $(brew --prefix)/opt/fzf/shell/completion.bash
-	fi
-fi
-if [ "${OSTYPE}" == "linux"* ]; then
-	[ -f ${HOME}/.fzf/shell/completion.bash ] && source "${HOME}/.fzf/shell/completion.bash" 2> /dev/null
-	[ -f ${HOME}/.fzf/shell/key-bindings.bash ] && source "${HOME}/.fzf/shell/key-bindings.bash"
-fi
+[[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.bash" 2> /dev/null
 
-if [[ -x "$(command -v fzf)" ]] && [[ -x "$(command -v ag)" ]]; then
-  export FZF_DEFAULT_COMMAND='ag --nocolor -g ""'
-  export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-  export FZF_ALT_C_COMMAND="$FZF_DEFAULT_COMMAND"
-  export FZF_DEFAULT_OPTS='
-  --color fg:242,bg:236,hl:65,fg+:15,bg+:239,hl+:108
-  --color info:108,prompt:109,spinner:108,pointer:168,marker:168
-  '
-fi
-
+# Key bindings
+# ------------
+source "$HOME/.fzf/shell/key-bindings.bash"
 
