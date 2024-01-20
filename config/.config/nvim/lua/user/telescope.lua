267ddb35c756c0ff6e1092255e0eb9f98bf793a3
Dan Kalowsky <dank@deadmime.org>
2024-01-20 11:26:08 -0800

    nvim: add telescope extension for fuzzzy finding
    Add the telescope tooling to the init.vim for the fuzzy finding
    capabilities of files and text.  This is likely going to need a few
    revisions to get right.
    
    Starting with a sample configuration from an online provided snapshot.
    
    Signed-off-by: Dan Kalowsky <dank@deadmime.org>

---
+++ config/.config/nvim/lua/user/telescope.lua
@@ -0,0 +1,137 @@
+local M = {
+  "nvim-telescope/telescope.nvim",
+  dependencies = {
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make", lazy = true },
+      { "nvim-lua/plenary.nvim" } },
+}
+
+function M.config()
+  local wk = require "which-key"
+  wk.register {
+    ["<leader>fb"] = { "<cmd>Telescope buffers previewer=false<cr>", "List Buffers" },
+    ["<leader>fc"] = { "<cmd>Telescope colorscheme<cr>", "List Colorschemes" },
+    ["<leader>ff"] = { "<cmd>Telescope find_files<cr>", "Find Files" },
+    -- yes this is duplicate until I can re-train my muscle memory to not use ctr-p
+    ["<C-p>"] = { "<cmd>Telescope find_files<cr>", "Find Files" },
+    ["<leader>ft"] = { "<cmd>Telescope live_grep<cr>", "Find Text" },
+    ["<leader>fh"] = { "<cmd>Telescope help_tags<cr>", "nvim Help" },
+    ["<leader>fl"] = { "<cmd>Telescope resume<cr>", "Last Search" },
+    ["<leader>fr"] = { "<cmd>Telescope oldfiles<cr>", "Recent Files" },
+    ["<leader>fa"] = { "<cmd>Telescope grep_string<cr>", "Find Cursor Text" },
+  }
+
+--  local icons = require "user.icons"
+  local actions = require "telescope.actions"
+
+
+  require("telescope").setup {
+    defaults = {
+      -- prompt_prefix = icons.ui.Telescope .. " ",
+      -- selection_caret = icons.ui.Forward .. " ",
+      entry_prefix = "   ",
+      initial_mode = "insert",
+      selection_strategy = "reset",
+      path_display = { "smart" },
+      color_devicons = true,
+      vimgrep_arguments = {
+        "rg",
+        "--color=never",
+        "--no-heading",
+        "--with-filename",
+        "--line-number",
+        "--column",
+        "--smart-case",
+        "--hidden",
+        "--glob=!.git/",
+      },
+
+      mappings = {
+        i = {
+          ["<C-n>"] = actions.cycle_history_next,
+          ["<C-p>"] = actions.cycle_history_prev,
+
+          ["<C-j>"] = actions.move_selection_next,
+          ["<C-k>"] = actions.move_selection_previous,
+        },
+        n = {
+          ["<esc>"] = actions.close,
+          ["j"] = actions.move_selection_next,
+          ["k"] = actions.move_selection_previous,
+          ["q"] = actions.close,
+        },
+      },
+    },
+    pickers = {
+      live_grep = {
+        theme = "dropdown",
+        previewer = true,
+      },
+
+      grep_string = {
+        theme = "dropdown",
+      },
+
+      find_files = {
+        theme = "dropdown",
+        previewer = true,
+        path_display = function(opts, path)
+            local tail = require("telescope.utils").path_tail(path)
+            return string.format("%s (%s)", tail, path)
+        end,
+      },
+
+      buffers = {
+        theme = "dropdown",
+        previewer = false,
+        initial_mode = "normal",
+        mappings = {
+          i = {
+            ["<C-d>"] = actions.delete_buffer,
+          },
+          n = {
+            ["dd"] = actions.delete_buffer,
+          },
+        },
+      },
+
+      planets = {
+        show_pluto = true,
+        show_moon = true,
+      },
+
+      colorscheme = {
+        enable_preview = true,
+      },
+
+      lsp_references = {
+        theme = "dropdown",
+        initial_mode = "normal",
+      },
+
+      lsp_definitions = {
+        theme = "dropdown",
+        initial_mode = "normal",
+      },
+
+      lsp_declarations = {
+        theme = "dropdown",
+        initial_mode = "normal",
+      },
+
+      lsp_implementations = {
+        theme = "dropdown",
+        initial_mode = "normal",
+      },
+    },
+    extensions = {
+      fzf = {
+        fuzzy = true, -- false will only do exact matching
+        override_generic_sorter = true, -- override the generic sorter
+        override_file_sorter = true, -- override the file sorter
+        case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+      },
+    },
+  }
+end
+
+return M
