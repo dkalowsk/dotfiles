e19bf18bb60d3b50b4ddec99fac5547c3d36ce86
Dan Kalowsky <dank@deadmime.org>
2024-01-21 15:18:50 -0800

    nvim: add lualine
    Not sure I have this configured right, but adding lualine
    and using copilot-line instead of the web-dev-icons
    as the dependency.
    
    It seems to work, but not sure I get the full line.
    
    Signed-off-by: Dan Kalowsky <dank@deadmime.org>

---
+++ config/.config/nvim/lua/user/lualine.lua
@@ -0,0 +1,29 @@
+local M = {
+    "nvim-lualine/lualine.nvim",
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+        opt = true,
+    },
+}
+
+function M.config()
+    require("lualine").setup {
+        options = {
+            icons_enabled = false,
+            theme = "horizon",
+            component_separators = { left = "▓▒░", right = "░▒▓" },
+            section_separators = { left = "▒░", right = "░▒" },
+            ignore_focus = { "NvimTree" },
+        },
+        sections = {
+            lualine_a = { "mode", "diff" },
+            lualine_b = { "branch", "diagnostics" },
+            lualine_c = { "filename" },
+            lualine_x = { "encoding", "fileformat", "filetype" },
+            lualine_y = { "progress" },
+            lualine_z = { "location" },
+        },
+    }
+end
+
+return M 
