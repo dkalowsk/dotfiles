1c783b459ffd006908b726d7bd3446eda2357adf
Dan Kalowsky <dank@deadmime.org>
2024-01-21 15:44:56 -0800

    nvim: add schemastore to list
    Apparently I might be missing the schemastore usage for the lspconfig
    capabilities.  Well not really schemastore, but the ability to download
    the schema as needed.  Schemastore looks like it does this.
    
    Signed-off-by: Dan Kalowsky <dank@deadmime.org>

--- config/.config/nvim/init.lua
+++ config/.config/nvim/init.lua
@@ -2,6 +2,8 @@ require "user.launch"
 require "user.options"
 require "user.keymaps"
 spec "user.colorscheme"
+-- schemastore needs to be before lspconfig so we can get the right schemea
+spec "user.schemastore"
 spec "user.lspconfig"
 -- cmp must come after lspconfig it seems
 spec "user.cmp"
