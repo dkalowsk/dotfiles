5e82c45ef7b3e3bf20dbe3ee3ee55a177a52591b
Dan Kalowsky <dank@deadmime.org>
2024-01-21 15:04:12 -0800

    nvim: add neogit
    Similar to fugitive, neogit tries to reimplement emacs magit tooling.
    We'll give it a try for awhile.
    
    Signed-off-by: Dan Kalowsky <dank@deadmime.org>

---
+++ config/.config/nvim/lua/user/neogit.lua
@@ -0,0 +1,37 @@
+local M = {
+    "NeogitOrg/neogit",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "sindrets/diffview.nvim",        -- optional - Diff integration
+
+        "nvim-telescope/telescope.nvim", -- optional
+    },
+    event = "VeryLazy",
+}
+
+function M.config()
+    local wk = require "which-key"
+    wk.register {
+        ["<leader>gg"] = { "<cmd>Neogit<CR>", "NeoGIT" },
+    }
+
+    require("neogit").setup {
+        auto_refresh = true,
+        disable_builtin_notifications = false,
+        use_magit_keybindings = false,
+        kind = "tab",
+        commit_popup = {
+            kind = "split",
+        },
+        popup = {
+            kind = "split",
+        },
+--        signs = {
+--            section = { icons.ui.ChevronRight, icons.ui.ChevronShortDown },
+--            item = { icons.ui.ChevronRIght, icons.ui.ChevronShortDown },
+--            hunk = { "", "" },
+--        },
+    }
+end
+
+return M
