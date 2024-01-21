d5d9a0187d868af9ab932567597e0ee13c707929
Dan Kalowsky <dank@deadmime.org>
2024-01-21 15:26:18 -0800

    nvim: adding nvim-cmp
    It looks like the LSP may not work fully without the cmp tooling for
    autocompletion of words, so bring it in.  Make sure this always loads
    after the lspconfig though as it seems before hand nothing gets
    triggered.
    
    Signed-off-by: Dan Kalowsky <dank@deadmime.org>

--- config/.config/nvim/init.lua
+++ config/.config/nvim/init.lua
@@ -3,6 +3,8 @@ require "user.options"
 require "user.keymaps"
 spec "user.colorscheme"
 spec "user.lspconfig"
+-- cmp must come after lspconfig it seems
+spec "user.cmp"
 spec "user.lualine"
 spec "user.neogit"
 spec "user.telescope"
