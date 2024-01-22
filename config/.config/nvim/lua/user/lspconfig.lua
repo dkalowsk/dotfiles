f2a7a5bcdeda4143839982160725b252f5d422ca
Dan Kalowsky <dank@deadmime.org>
2024-01-22 09:10:13 -0800

    nvim: add gitsigns
    Make it easier to interact with git within the editor.  This is more a
    fugitive compliment but since I'm not a big user of fugitive it's more a
    replacement for me.
    
    Signed-off-by: Dan Kalowsky <dank@deadmime.org>

--- config/.config/nvim/lua/user/lspconfig.lua
+++ config/.config/nvim/lua/user/lspconfig.lua
@@ -62,24 +62,26 @@ function M.config()
     }
 
     local lspconfig = require "lspconfig"
+    local icons = require "user.icons"
 
     local servers = {
         "bashls",
         "clang-format",
         "clangd",
+        "lua_ls",
         "pyright",
     }
 
     local default_diagnostic_config = {
---        signs = {
---            active = true,
---            values = {
---                { name  = "DiagnosticSignError", text = icons.diagnostics.Error },
---                { name  = "DiagnosticSignWarn", text = icons.diagnostics.Warning },
---                { name  = "DiagnosticSignHint", text = icons.diagnostics.Hint },
---                { name  = "DiagnosticSignInfo", text = icons.diagnostics.Information },
---            },
---        },
+        signs = {
+            active = true,
+            values = {
+                { name  = "DiagnosticSignError", text = icons.diagnostics.Error },
+                { name  = "DiagnosticSignWarn", text = icons.diagnostics.Warning },
+                { name  = "DiagnosticSignHint", text = icons.diagnostics.Hint },
+                { name  = "DiagnosticSignInfo", text = icons.diagnostics.Information },
+            },
+        },
         virtual_text = false,
         update_in_insert = false,
         underline = true,
@@ -96,9 +98,9 @@ function M.config()
 
     vim.diagnostic.config(default_diagnostic_config)
 
- --   for _, sign in ipairs(vim.tbl_get(vim.diagnostic.config(), "signs", "values") or {}) do
- --       vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = sign.name })
- --   end
+    for _, sign in ipairs(vim.tbl_get(vim.diagnostic.config(), "signs", "values") or {}) do
+        vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = sign.name })
+    end
 
     vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
     vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
