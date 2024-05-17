86bf82000fb2556621278925a62f9015556e6d76
Dan Kalowsky <dank@deadmime.org>
2024-05-17 09:37:24 -0700

    nvim: osc52: reduce usage
    While all of this works really well for remot SSH copy,
    on a local system it breaks copying to the clipboard
    and instead copies the whole file... ocassionally.
    
    I did something bad, but I can't figure out what it is
    so for now revert some of it.
    
    Hopefully this can all go away when updating to nvim v10
    where osc52 is native to the system.
    
    Signed-off-by: Dan Kalowsky <dank@deadmime.org>

--- config/.config/nvim/lua/user/osc52.lua
+++ config/.config/nvim/lua/user/osc52.lua
@@ -2,30 +2,10 @@ local M = {
     "ojroques/nvim-osc52"
 }
 
-local function copy(lines, _)
-  require('osc52').copy(table.concat(lines, '\n'))
-end
-
-local function paste()
-  return {vim.fn.split(vim.fn.getreg(''), '\n'), vim.fn.getregtype('')}
-end
-
 function M.config()
     vim.keymap.set('n', '<leader>c', require('osc52').copy_operator, {expr = true})
     vim.keymap.set('n', '<leader>cc', '<leader>c_', {remap = true})
     vim.keymap.set('v', '<leader>c', require('osc52').copy_visual)
-
-    vim.g.clipboard = {
-      name = 'osc52',
-      copy = {['+'] = copy, ['*'] = copy},
-      paste = {['+'] = paste, ['*'] = paste},
-    }
-    require("osc52").setup {
-        max_length = 0,  -- maximum length of selection, 0 means no limit
-        silent = false,  -- disable message on successful copy
-        trim = false,    -- trim surrounding whitespaces before copy
-        tmux_passthrough = false, -- use tmux?  set this to true 
-    }
 end
 
 return M
