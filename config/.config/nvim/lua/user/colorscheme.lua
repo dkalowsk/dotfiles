5f060c0495b0309610d5c027de94d340952b0cb7
Dan Kalowsky <dank@deadmime.org>
2024-07-16 18:15:36 -0700

    nvim: colorscheme: add details for tokyonight
    Add some of configuration details for tokyonight as
    described on the GitHub page.
    
    Signed-off-by: Dan Kalowsky <dank@deadmime.org>

--- config/.config/nvim/lua/user/colorscheme.lua
+++ config/.config/nvim/lua/user/colorscheme.lua
@@ -6,6 +6,20 @@ local M = {
 
 function M.config()
     vim.cmd.colorscheme "tokyonight-night"
+    require("tokyonight").setup({
+      -- use the night style
+      style = "night",
+      -- disable italic for functions
+      styles = {
+        functions = {}
+      },
+      sidebars = { "qf", "vista_kind", "terminal", "packer" },
+      -- Change the "hint" color to the "orange" color, and make the "error" color bright red
+      on_colors = function(colors)
+        colors.hint = colors.orange
+        colors.error = "#ff0000"
+      end
+    })
 end
 
 return M
