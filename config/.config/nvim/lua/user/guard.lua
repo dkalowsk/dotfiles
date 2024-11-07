503672c5d6ab3c1bb9c795e38712bbfd091d8957
Dan Kalowsky <dank@deadmime.org>
2024-11-06 16:50:24 -0800

    config: nvim: fix guard includes
    The newer guard no longer supports the 'setup' command and
    instead requests that the vim.g.guard_config be used.
    
    Convert over to the newer config.
    
    Signed-off-by: Dan Kalowsky <dank@deadmime.org>

--- config/.config/nvim/lua/user/guard.lua
+++ config/.config/nvim/lua/user/guard.lua
@@ -15,7 +15,7 @@ function M.config()
     ft('py'):fmt('black')
 
 
-    require("guard").setup {
+    vim.g.guard_config = {
         fmt_on_save = true,
         lsp_as_default_formatter = false
     }
