8e3356d32084d2583a345a4203f05de989d74e5e
Dan Kalowsky <dkalowsk@qli.qualcomm.com>
2026-05-14 22:53:57 +0000

    config: nvim: move to astronvim
    I've been maintaining my own configuration files for awhile
    and while it's fun it's a lot of effort to keep things working.
    
    Recently moved over to astronvim, and while it's not 100% great
    it covers enough that I'm enjoying using it.  Most of it has
    already matched to my already established key commands so it's
    not a huge change to the muscle memory.
    
    Signed-off-by: Dan Kalowsky <dkalowsk@qli.qualcomm.com>

---
+++ config/.config/nvim/lua/community.lua
@@ -0,0 +1,15 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  -- import/override with your plugins folder
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
+  { import = "astrocommunity.colorscheme.nightfox-nvim" },
+  { import = "astrocommunity.colorscheme.kanagawa-nvim" },
+  { import = "astrocommunity.fuzzy-finder.fzf-lua" },
+  { import = "astrocommunity.git.neogit" },
+}
